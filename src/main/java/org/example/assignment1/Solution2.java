f710f911326b2123aab20b232840f8b0fa8c9cda
KaterinaN86 <katerinagj@gmail.com>
2022-11-14 17:52:20 +0100

    Refactored solution 2

--- src/main/java/org/example/assignment1/Solution2.java
+++ src/main/java/org/example/assignment1/Solution2.java
@@ -26,43 +26,25 @@ public class Solution2 {
         this.word = word;
     }
 
-    private int findIndexOfLetter(char letter, String s) {
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == letter)
-                //once the letter is found the index is returned an execution stops
-                return i;
-        }
-
-        return -1;
-    }
 
     private String trimInputString(String s, int index) {
 
-        ArrayList<Character> list = new ArrayList<Character>();
-        for (char c : s.toCharArray()) {
-            list.add(c);
-        }
-        list.remove(index);
-        String result = "";
-        for (Character temp : list) {
-            result += temp + "";
-        }
 
-        return result;
+        return s.substring(0,index)+s.substring(index+1);
     }
 
     Pair<String, Boolean> shortenInputString(String input, String word){
         String result = input;
         int count = 0;
         for(char letter : word.toCharArray()){
-            int index = findIndexOfLetter(letter,input);
+            int index = input.indexOf(letter);
             if(index!=-1){
                 result = trimInputString(input, index );
                 count++;
             }
         }
 
-        boolean shortenWholeWord = input.length()==count?true:false;
+        boolean shortenWholeWord = word.length()==count;
 
         return new Pair<String,Boolean>(result,shortenWholeWord);
     }
